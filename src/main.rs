1f1c7be0e320d4061cbb87dae54cab4f3bafcaeb
Ben PHL <benphawke@gmail.com>
2023-08-28 19:04:30 +0200

    clean up working reference impl

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,35 @@
-
-
 fn main() {
-    let types = vec!["u8", "bool" , "f32", "Foo", "Bar", "Baz"];
-    let gen = gen_where_line(("".to_string(), 0), &types);
-    dbg!(gen);
+    let types = vec!["u8", "bool", "f32", "Foo", "Bar", "Baz"];
+    let wheres = (0..types.len()).map(|i| {
+
+        gen_where_line(("".to_string(), 0), &types[0..=i])
+    }).collect::<Vec<_>>();
+    dbg!(wheres);
 }
 
-/*
-L0: Plucker<GpioPin<Unknown, 4>, L1>,
-<L0 as Plucker<u8, L1>>::Remainder: Plucker<GpioPin<Unknown, 5>, L2>,
-
-<<L0 as Plucker<GpioPin<Unknown, 4>, L1>>::Remainder as Plucker<GpioPin<Unknown, 5>, L2>>::Remainder: Plucker<GpioPin<Unknown, 6>, L3>,
-3, 3
-: Plucker<GpioPin<Unknown, 6>, L3>,
-2, 3: constraint handled
-<<L0 as Plucker<GpioPin<Unknown, 4>, L1>>::Remainder as Plucker<GpioPin<Unknown, 5>, L2>>::Remainder
-L0 (as)
-Plucker<fst, L1>
-^^::Remain (as)
-Plucker<snd, L2>
-^^::Remain (as)
-Plucker<thd, L3>
-^^::Remainder - for the result
-
- * */
-
-fn gen_where_line(mut acc: ( String, u8 ), types: &[&'static str]) -> String {
+fn gen_where_line(mut acc: (String, u8), types: &[&'static str]) -> String {
     if types.len() == 0 {
         return acc.0;
     }
 
     let next = next_type(&mut acc, types[0]);
     gen_where_line((next, acc.1 + 1), &types[1..])
-
 }
 
 fn next_type(curr: &mut (String, u8), next: &'static str) -> String {
     // not the start: bump it over
     if curr.0.contains(": Plucker") {
-        return format!("<{}>::Remainder: Plucker<{}, L{}>", curr.0.replace(": Plucker", " as Plucker"), next, curr.1 + 1);
+        return format!(
+            "<{}>::Remainder: Plucker<{}, L{}>",
+            // treat this list as what's left after a pluck
+            curr.0.replace(": Plucker", " as Plucker"),
+            // make sure what's left after the pluck can pluck the next type
+            next,
+            // ...and the resulting list following said pluck, is a known-list
+            curr.1 + 1
+        );
     }
 
     // start, so load the first type
-    format!("L0: Plucker<{}, L1>", next) 
-    
+    format!("L0: Plucker<{}, L1>", next)
 }
-    
