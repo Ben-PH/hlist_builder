bd6a1dc37aa96bf3a4828f16bf5ae41c49757d73
Ben PHL <benphawke@gmail.com>
2023-08-28 21:47:01 +0200

    generating: fn hl_new<...>(...)->(Self, XXX)XXX{}

--- hl_build_macro/src/lib.rs
+++ hl_build_macro/src/lib.rs
@@ -1,26 +1,104 @@
 extern crate proc_macro;
-
 use proc_macro::TokenStream;
-use quote::quote;
-use syn::{parse_macro_input, DeriveInput, Type};
-
+use quote::{quote, ToTokens};
+use syn::{
+    parse_macro_input, Block, DeriveInput, FnArg, Generics, Pat, PatIdent, PatType, Type, TypeParam, punctuated::Punctuated,
+};
 
 #[proc_macro_attribute]
 pub fn hl_build(_attr: TokenStream, item: TokenStream) -> TokenStream {
     let mut input = parse_macro_input!(item as DeriveInput);
-    let struct_ident: &syn::Ident = &input.ident;
 
-    let hl_build_fields = field_vec(&input.data);
+    let mut annotated_fields: Vec<(syn::Ident, syn::Type)> = Vec::new();
+    let mut non_annotated_fields: Vec<(syn::Ident, syn::Type)> = Vec::new();
 
-
-    // Modify the `input` to remove `hl_build` attributes from fields
-    if let syn::Data::Struct(syn::DataStruct { fields: syn::Fields::Named(named), .. }) = &mut input.data {
+    if let syn::Data::Struct(syn::DataStruct {
+        fields: syn::Fields::Named(named),
+        ..
+    }) = &mut input.data
+    {
         for field in &mut named.named {
-            field.attrs.retain(|attr| !attr.path().is_ident("hl_field"));
+            let is_annotated = field
+                .attrs
+                .iter()
+                .any(|attr| attr.path().is_ident("hl_field"));
+
+            let field_ident = field.ident.clone().unwrap(); // Assuming named fields
+            let field_type = field.ty.clone(); // Type
+
+            if is_annotated {
+                annotated_fields.push((field_ident, field_type));
+                // Remove the hl_field attribute
+                field.attrs.retain(|attr| !attr.path().is_ident("hl_field"));
+            } else {
+                non_annotated_fields.push((field_ident, field_type));
+            }
         }
     }
 
+    let args = non_annotated_fields
+        .iter()
+        .map(|(ident, ty)| {
+            let pat_id = PatIdent {
+                attrs: vec![],
+                by_ref: None,
+                mutability: None,
+                ident: ident.clone(),
+                subpat: None,
+            };
+            let pat_tp = PatType {
+                attrs: vec![],
+                pat: Box::new(Pat::Ident(pat_id)),
+                colon_token: Default::default(),
+                ty: Box::new(ty.clone()),
+            };
+            FnArg::Typed(pat_tp)
+        })
+        .collect::<Vec<_>>();
 
+    let gens: String = annotated_fields
+        .iter()
+        .enumerate()
+        .map(|(i, _)| format!("L{}", i))
+        .collect::<Vec<String>>()
+        .join(", ");
+    let gens = format!("<{}>", gens);
+    let parsed_gens: syn::Generics = syn::parse_str(&gens).unwrap();
+    let where_clause: syn::WhereClause = syn::parse_str("where Foo: Bar").unwrap();
+    let fn_ident = syn::Ident::new("hl_new", proc_macro2::Span::call_site());
+    let output: syn::ReturnType = syn::parse_str("-> (Self, Fizz)").unwrap();
+    let fn_arg: syn::FnArg = syn::parse_str("l0: L0").unwrap();
+    let args = std::iter::once(fn_arg).chain(args);
+
+    let sig = syn::Signature {
+        ident: fn_ident,
+        generics: Generics {
+            where_clause: Some(where_clause),
+            params: parsed_gens.params,
+            ..Default::default()
+        },
+        inputs: Punctuated::from_iter(args.into_iter()),
+
+        output,
+        constness: None,
+        asyncness: None,
+        unsafety: None,
+        abi: None,
+        variadic: None,
+        fn_token: Default::default(),
+        paren_token: Default::default(),
+    };
+    let block = Box::new(syn::parse_str::<Block>("{ /* function body */ }").unwrap());
+    let fun = syn::ItemFn {
+        attrs: vec![],
+        vis: syn::Visibility::Inherited,
+        sig,
+        block,
+    };
+    dbg!(quote! {#fun}.to_string());
+    let mut struct_tokens = proc_macro2::TokenStream::new();
+    input.to_tokens(&mut struct_tokens);
+    println!("Parsed Struct:\n{}", struct_tokens.to_string());
     todo!();
     //expanded.into()
 }
@@ -28,11 +106,19 @@ pub fn hl_build(_attr: TokenStream, item: TokenStream) -> TokenStream {
 fn field_vec(inner: &syn::Data) -> Vec<(syn::Ident, syn::Type)> {
     let mut res: Vec<(syn::Ident, syn::Type)> = Vec::new();
 
-    if let syn::Data::Struct(syn::DataStruct { fields: syn::Fields::Named(named), .. }) = inner {
+    if let syn::Data::Struct(syn::DataStruct {
+        fields: syn::Fields::Named(named),
+        ..
+    }) = inner
+    {
         for field in named.named.iter() {
-            if field.attrs.iter().any(|attr| attr.path().is_ident("hl_field")) {
+            if field
+                .attrs
+                .iter()
+                .any(|attr| attr.path().is_ident("hl_field"))
+            {
                 let field_ident = field.ident.clone().unwrap(); // Assuming named fields, not
-                                                               // tuple-struct
+                                                                // tuple-struct
                 let field_type = field.ty.clone(); // Type
                 res.push((field_ident, field_type));
             }
@@ -40,4 +126,3 @@ fn field_vec(inner: &syn::Data) -> Vec<(syn::Ident, syn::Type)> {
     }
     res
 }
-
--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,40 @@
-use frunk::hlist;
+use frunk::hlist::Plucker;
 
+#[hl_build_macro::hl_build]
+struct Blinker {
+    #[hl_field]
+    field0: u8,
+    #[hl_field]
+    field1: bool,
+    field3: f32,
+}
 
-
-// #[hl_build_macro::hl_build]
-// struct Blinker {
-// 
-//     #[hl_field]
-//     pin4: u8,
-// 
-//     #[hl_field]
-//     pin5: u8,
+// impl Blinker {
+//     fn hl_new<L0, L1, L2>(
+//         l0: L0,
+//         field3: f32,
+//     ) -> (
+//         Blinker,
+//         <<L0 as Plucker<u8, L1>>::Remainder as Plucker<bool, L2>>::Remainder,
+//     )
+//     where
+//         L0: Plucker<u8, L1>,
+//         <L0 as Plucker<u8, L1>>::Remainder: Plucker<bool, L2>,
+//     {
+//         let (pin4, l1) = l0.pluck();
+//         let (pin5, l2) = l1.pluck();
+//         (
+//             Blinker {
+//                 field0: pin4,
+//                 field1: pin5,
+//                 field3,
+//             },
+//             l2,
+//         )
+//     }
 // }
-// 
-// fn foo() {
-//     let list = hlist!(3u8, true, String::from("list-str"), 10.4);
-//     let builder = BlinkerBuilder::new(list);
-//     let builder = builder.set_pin4();
-//     let builder = builder.set_pin5();
-//     let (built, new_list) = builder.build();
-// }
+
+fn foo() {
+    let list = frunk::hlist!(true, 3u8, String::from("list-str"), 10.4);
+    let (blinker, list) = Blinker::hl_new(list, 69.420);
+}
