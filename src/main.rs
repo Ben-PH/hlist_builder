bd1ab488e5f06472453307565c9c5de1ad6ce77c
Ben PHL <benphawke@gmail.com>
2023-08-28 02:45:50 +0200

    it's working.meme

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 
 
 fn main() {
-    let types = vec!["u8".to_string() , "bool".to_string(), "f32".to_string()];
-    let gen = gen_where_line(0, 3, &types);
+    let types = vec!["u8", "bool" , "f32", "Foo", "Bar", "Baz"];
+    let gen = gen_where_line(("".to_string(), 0), &types);
     dbg!(gen);
 }
 
@@ -24,33 +24,25 @@ Plucker<thd, L3>
 ^^::Remainder - for the result
 
  * */
-fn gen_where_line(list_n: usize, last_post: usize, types: &[String]) -> String {
-    // first, set up the "what it must impl
-    if list_n == last_post{
-        return format!(": {},\n", plucker(&types[last_post - 1], list_n));
-    }
-    if last_post == 1 {
-        return format!("L0{}", gen_where_line(list_n - 1, last_post, types));
+
+fn gen_where_line(mut acc: ( String, u8 ), types: &[&'static str]) -> String {
+    if types.len() == 0 {
+        return acc.0;
     }
 
-    format!("{}{}", cur_type(list_n, types), gen_where_line(list_n + 1, last_post, types))
+    let next = next_type(&mut acc, types[0]);
+    gen_where_line((next, acc.1 + 1), &types[1..])
+
 }
 
-fn cur_type(list_n: usize, types: &[String]) -> String {
-    if list_n == 0 {
-        return "L0".to_string();
+fn next_type(curr: &mut (String, u8), next: &'static str) -> String {
+    // not the start: bump it over
+    if curr.0.contains(": Plucker") {
+        return format!("<{}>::Remainder: Plucker<{}, L{}>", curr.0.replace(": Plucker", " as Plucker"), next, curr.1 + 1);
     }
-    format!("<{} as {}>::Remainder", cur_type(list_n - 1, types), plucker(&types[list_n - 1], list_n))
-}
 
-fn alias(origin: &String, alias: Option<&String>) -> String {
-    let Some(alias) = alias else {
-        return origin.clone();
-    };
-    format!("<{} as {}>::Remainder", origin, alias)
+    // start, so load the first type
+    format!("L0: Plucker<{}, L1>", next) 
+    
 }
-
-
-fn plucker(tp: &String, list_n: usize) -> String {
-    format!("Plucker<{}, L{}>", tp, list_n )
-}
+    
